a7b416aa3cb02c308fbaef5c7c38470c7454b250
linha-de-sintra <buedanegro@gmail.com>
2021-08-12 18:59:28 +0100

    update de ui

--- pages/historia.js
+++ pages/historia.js
@@ -1,3 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import footer from '../comps/footer'
+
 export default function Home() {
-    return <h1>Em construção...</h1>
+    return(
+        <>
+        <Head>
+          <title>Situação do Daniel</title>
+          <meta name="description" content="As condições a que são sujeitos os jovens inscritos na instituição" />
+          <link rel="icon" href="/dani.bmp" />
+        </Head>
+        <main className={styles.main}>
+        <h1 className={styles.title}>
+          Situação do Daniel</h1>
+</main>
+        <p className={styles.description}>
+          As condições a que são sujeitos os jovens inscritos na instituição
+        </p>)
+        <span className={styles.logo}>
+            <Image src="/merda.jpg" alt="graffiti nas instalações" width={868} height={1156} />
+            <footer />
+            </span>
+</>
+  )
 }
